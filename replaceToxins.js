965a29144cd04df2e6b7231f6986f8493a132342
bgarvey <bgarvey@rjmetrics.com>
2015-11-22 08:39:23 -0500

    giving up on facebook

--- replaceToxins.js
+++ replaceToxins.js
@@ -1,4 +1,10 @@
-console.log("Scanning for toxins");
-var replaced = $("body").html().replace(/toxin(s)+/ig,'Dark Magic');
-$("body").html(replaced);
-console.log("Scan complete");
+
+// Can't get it to work on facebook yet
+if (!isFacebook()) {
+  var replacedxyz = $("body").html().replace(/toxin(s){0,1}/ig,'Dark Magic');
+  $("body").html(replacedxyz);
+}
+
+function isFacebook() {
+  return window.location.hostname.match(/facebook/) != null;
+}
